d318c0eff03c7e96e470941acd13c38834e8d532
madanihoussem <madanihoussem98@gmail.com>
2022-07-13 16:12:13 +0200

    HomePage updated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, ContactRepository $contactRepository): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactRepository->add($contact, true);
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'contactForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -16,11 +19,34 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('email')
-            ->add('objet')
-            ->add('message')
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-floating mb-3',
+                    'placeholder' => 'Nom',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-floating mb-3',
+                    'placeholder' => 'Email',
+                ],
+            ])
+            ->add('objet', TextType::class, [
+                'attr' => [
+                    'class' => 'form-floating mb-3',
+                    'placeholder' => 'Object',
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-floating mb-3',
+                    'placeholder' => 'Message',
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'attr' => [
+                    'class' => 'mb-3',
+                ],
                 'mapped' => false,
                 'label' => "Terms & conditions of data collection",
                 'constraints' => [
