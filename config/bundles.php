d7605d59b9d98fbeec343c074a2e57e1da4caa4f
madanihoussem <madanihoussem98@gmail.com>
2022-10-15 17:41:10 +0200

    test github secrets on env

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
