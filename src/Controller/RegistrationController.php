5328b10b3004f2d5413078f8b57d24ab46de661c
madanihoussem <59016172+madanihoussem@users.noreply.github.com>
2023-06-28 10:09:02 +0200

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -46,7 +46,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('madanihoussem98@gmail.com', "DevBuddy's"))
+                    ->from(new Address('bonjour@marchedici.fr', "DevBuddy's"))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email - DevBuddy')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
